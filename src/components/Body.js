3867f3ecf7a28a50038f9f0590d72535948fa04a
santoshsharma27 <santosh37kr@gmail.com>
2023-05-27 22:18:11 +0530

    Episode 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,6 @@
-import RestaurantCard from "./Restaurant";
-import resList from "./utils/mockData";
-import { useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 // Body Component for body section: It contain all restaurant cards
 // We are mapping restaurantList array and passing data to RestaurantCard component as props with unique key as index
@@ -8,25 +8,68 @@ import { useState } from "react";
 const Body = () => {
   // Local State Behaviour - Super powerful variable
 
-  const [listofRestaurants, setlistofRestaurants] = useState(resList);
+  const [listofRestaurants, setListofRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
-  return (
+  useEffect(() => {
+    console.log("useEffect called");
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9046136&lng=77.614948&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    // Optional Chaining
+    setListofRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+  };
+
+  return listofRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            className=""
+            type="text"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              console.log(searchText);
+
+              const filteredRestaurant = listofRestaurants.filter((res) =>
+                res.data.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
             const filteredList = listofRestaurants.filter(
               (res) => res.data.avgRating > 4
             );
-            setlistofRestaurants(filteredList);
+            setListofRestaurants(filteredList);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {listofRestaurants.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
---
+++ src/components/Shimmer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Shimmer = () => {
+  return (
+    <div className="shimmer-container">
+      {Array(15)
+        .fill("")
+        .map((e, index) => (
+          <div key={index} className="shimmer-card"></div>
+        ))}
+    </div>
+  );
+};
+
+export default Shimmer;
