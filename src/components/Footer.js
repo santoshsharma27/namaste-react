008c18ac282702b2cef17ffb3f13dd28c76ba926
santoshsharma27 <santosh37kr@gmail.com>
2023-06-03 19:57:49 +0530

    Added footer code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,17 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <h4 className="footer">This site is developed by Santosh</h4>
+      <h4 className="footer">
+        This site is developed by
+        <a
+          href="https://www.linkedin.com/in/santosh-kumar-sharma-15b132168/"
+          target="_blank"
+          title="Santosh Kumar Sharma Linkedin Profile"
+        >
+          Santosh Kumar Sharma
+        </a>
+        <span>🚀 2023 Food Fire</span>
+      </h4>
     </div>
   );
 };
