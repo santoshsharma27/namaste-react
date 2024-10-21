32f575a694af4aea52b80fdd2907419e3dca8d41
santoshsharma27 <santosh37kr@gmail.com>
2024-10-21 19:43:43 +0530

    Fixed menu api url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,7 +5,7 @@ export const SWIGGY_API =
   "https://foodfire.onrender.com/api/restaurants?lat=12.9046136&lng=77.614948&page_type=DESKTOP_WEB_LISTING";
 
 export const MENU_API =
-  "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.99740&lng=79.00110&restaurantId=";
+  "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.99740&lng=79.00110&&submitAction=ENTER&restaurantId=";
 
 export const USERNAME = "Santosh";
 
@@ -16,3 +16,5 @@ export const Linkedin_URL = "https://www.linkedin.com/in/santoshsharma37/";
 
 // export const MENU_API =
 //   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.99740&lng=79.00110&restaurantId=";
+
+// https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId
